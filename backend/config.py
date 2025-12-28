41cde266c46d58255185f5e79a0e5c371559a547
RudraSuthar-web <rudracodelearner@gmail.com>
2025-12-28 16:53:06 +0530

    new-frontend

--- backend/config.py
+++ backend/config.py
@@ -2,10 +2,10 @@ import os
 from google import genai
 
 # Setup API Key (Best practice: use environment variables, but this works for now)
-API_KEY = os.getenv("GEMINI_API_KEY", "AIzaSyBTU3pGP5LYnoHJfRoq6Q3yYAOaMWULOsU")
+API_KEY = os.getenv("GEMINI_API_KEY")#after "GEMINI_API_KEY",
 
 # Initialize Client once here to import elsewhere
 client = genai.Client(api_key=API_KEY)
 
 # Model configuration
-MODEL_NAME = "gemini-3-flash-preview" # Upgraded to 2.0 Flash (faster/better)
+MODEL_NAME = "gemini-2.5-flash" # Upgraded to 2.0 Flash (faster/better)
